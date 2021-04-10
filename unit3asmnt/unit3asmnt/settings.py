bdc4a2a5d1afde85d01c1727202837d4c176615d
Logan <mlogan858@hotmail.com>
2021-04-09 21:18:38 -0400

    gitignore

--- unit3asmnt/unit3asmnt/settings.py
+++ unit3asmnt/unit3asmnt/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+environ.Env()
+environ.Env.read_env()
+import os
+my_key = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g$@5hn-9o-40=6cv2rw_g8jn4+haxaex_%*^&+13qi4lih1o01'
+SECRET_KEY = my_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
